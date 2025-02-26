c27b65af8f6d9e5e064ff47d0cd952ef79454af3
psiblvdegod <artyombaly@list.ru>
2025-02-26 17:12:36 +0300

    feat : add class Trie implemetation; feat : add tests for .Add(), .Remove()

---
+++ Trie/Tests/TrieTests.cs
@@ -0,0 +1,82 @@
+﻿namespace Tests;
+
+using Trie;
+
+[TestFixture]
+public class TrieTests
+{
+    [Test]
+    public void Constructor_SequenceAsInput()
+    {
+        List<string> sequence = ["first", "second", "third"];
+
+        var trie = new Trie(sequence);
+
+        for (var i = 0; i < sequence.Count; ++i)
+        {
+            Assert.That(trie.DoesContain(sequence[i]));
+        }
+    }
+
+    [Test]
+    public void Add_OneElementAsInput()
+    {
+        var trie = new Trie();
+
+        Assert.That(trie.Add("string"));
+
+        Assert.That(trie.DoesContain("string"));
+    }
+
+    [Test]
+    public void Add_SequenceAsInput()
+    {
+        var trie = new Trie();
+
+        List<string> sequence = ["first", "second", "third"];
+
+        for (var i = 0; i < sequence.Count; ++i)
+        {
+            Assert.That(trie.Add(sequence[i]));
+        }
+
+        for (var i = 0; i < sequence.Count; ++i)
+        {
+            Assert.That(trie.DoesContain(sequence[i]));
+        }
+    }
+
+    [Test]
+    public void Add_EmptyStringAsInput()
+    {
+        var trie = new Trie();
+
+        Assert.Throws<ArgumentException>(() => trie.Add(string.Empty));
+    }
+
+    [Test]
+    public void Remove_ElementIsInTrie()
+    {
+        var trie = new Trie(["element"]);
+
+        Assert.That(trie.Remove("element"));
+
+        Assert.That(!trie.DoesContain("element"));
+    }
+
+    [Test]
+    public void Remove_ElementIsNotInTrie()
+    {
+        var trie = new Trie();
+
+        Assert.That(!trie.Remove("element"));
+    }
+
+    [Test]
+    public void Remove_EmptyStringAsInput()
+    {
+        var trie = new Trie();
+
+        Assert.Throws<ArgumentException>(() => trie.Remove(string.Empty));
+    }
+}
---
+++ Trie/Trie/Trie.cs
@@ -0,0 +1,196 @@
+﻿
+namespace Trie
+{
+    /// <summary>
+    /// Implements the "Trie" data structure.
+    /// </summary>
+    public class Trie
+    {
+        private readonly Vertex root;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Trie"/> class.
+        /// </summary>
+        public Trie()
+        {
+            this.root = new('/');
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Trie"/> class.
+        /// </summary>
+        /// <param name="elements">The sequence from which the Trie is created.</param>
+        public Trie(IEnumerable<string> elements)
+            : this()
+        {
+            foreach (var element in elements)
+            {
+                this.Add(element);
+            }
+        }
+
+        /// <summary>
+        /// Gets amount of elements in the Trie.
+        /// </summary>
+        public int Size
+        {
+            get { return this.root.HeirsNumber; }
+        }
+
+        /// <summary>
+        /// Adds element to the Trie.
+        /// </summary>
+        /// <returns>true if item is successfully added; otherwise, false.</returns>
+        /// <param name="element">The element which will be added.</param>
+        public bool Add(string element)
+        {
+            ArgumentException.ThrowIfNullOrEmpty(element);
+
+            List<Vertex> path = [];
+            var current = this.root;
+            path.Add(current);
+
+            foreach (var c in element)
+            {
+                var next = current.Find(c);
+
+                if (next is null)
+                {
+                    next = new Vertex(c);
+                    current.Link(next);
+                }
+
+                path.Add(next);
+                current = next;
+            }
+
+            if (current.IsInTrie)
+            {
+                return false;
+            }
+
+            current.IsInTrie = true;
+
+            foreach (var v in path)
+            {
+                ++v.HeirsNumber;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Removes element from the Trie.
+        /// </summary>
+        /// <returns>true if item is successfully removed; otherwise, false.</returns>
+        /// <param name="element">The element which will be removed.</param>
+        public bool Remove(string element)
+        {
+            ArgumentException.ThrowIfNullOrEmpty(element);
+
+            List<Vertex> path = [];
+            var current = this.root;
+            path.Add(current);
+
+            foreach (var c in element)
+            {
+                var next = current.Find(c);
+
+                if (next is null)
+                {
+                    return false;
+                }
+
+                path.Add(next);
+                current = next;
+            }
+
+            if (!current.IsInTrie)
+            {
+                return false;
+            }
+
+            current.IsInTrie = false;
+
+            for (var i = 1; i < path.Count; ++i)
+            {
+                if (path[i].HeirsNumber == 1)
+                {
+                    path[i - 1].Unlink(path[i]);
+                    break;
+                }
+
+                --path[i].HeirsNumber;
+            }
+
+            --this.root.HeirsNumber;
+
+            return true;
+        }
+
+        /// <summary>
+        /// Finds element in the Trie.
+        /// </summary>
+        /// <returns>true if item was found; otherwise, false.</returns>
+        /// <param name="element">The element which will be searched for.</param>
+        public bool DoesContain(string element)
+        {
+            var current = this.root;
+
+            foreach (var c in element)
+            {
+                var next = current.Find(c);
+
+                if (next is null)
+                {
+                    return false;
+                }
+
+                current = next;
+            }
+
+            return current.IsInTrie;
+        }
+
+        /// <summary>
+        /// Counts word with such prefix in the Trie.
+        /// </summary>
+        /// <returns>Number of elements, which have such prefix.</returns>
+        /// <param name="prefix">The prefix which defines search key.</param>
+        public int CountWordsWithSuchPrefix(string prefix)
+        {
+            var current = this.root;
+
+            foreach (var c in prefix)
+            {
+                var next = current.Find(c);
+
+                if (next is null)
+                {
+                    return 0;
+                }
+
+                current = next;
+            }
+
+            return current.HeirsNumber;
+        }
+
+        private class Vertex(char symbol, bool isInTrie = false)
+        {
+            private readonly List<Vertex> linked = [];
+
+            internal char Symbol { get; } = symbol;
+
+            internal bool IsInTrie { get; set; } = isInTrie;
+
+            internal int HeirsNumber { get; set; }
+
+            internal void Link(Vertex vertex) => this.linked.Add(vertex);
+
+            internal bool Unlink(Vertex vertex) => this.linked.Remove(vertex);
+
+            internal Vertex? Find(char symbol) => this.linked.Find(x => x.Symbol == symbol);
+        }
+    }
+}
