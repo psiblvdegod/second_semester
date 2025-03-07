298409b6a92d91ed9d742320ffabb8e063bd4955
psiblvdegod <artyombaly@list.ru>
2025-03-08 02:42:55 +0300

    feat : add file interaction with Compress() to Program.cs

--- Trie/Program/Program.cs
+++ Trie/Program/Program.cs
@@ -1,4 +1,40 @@
-﻿using LZW;
-using static LZW.FileProcessing.FileProcessing;
+﻿using static LZW.LZW;
+using System.Linq.Expressions;
 
-StartProgram();
+if (args.Length != 2)
+{
+    Console.WriteLine("Specify params.");
+    return -1;
+}
+
+if (args[1] == "c")
+{
+    var input = File.ReadAllText(args[0]);
+
+    var result = Compress(input);
+
+    var dir = args[0][(args[0].LastIndexOf('/') + 1)..];
+
+    var stream = File.CreateText(dir + ".zipped");
+
+    try
+    {
+        stream.Write(result);
+    }
+    finally
+    {
+        stream.Close();
+    }
+}
+
+if (args[1] == "-u")
+{
+    if (args[0][args[0].LastIndexOf('.')..] != ".zipped")
+    {
+        throw new ArgumentException("Invalid file format.");
+    }
+
+    var input = File.ReadAllText(args[0]);
+}
+
+return 0;
