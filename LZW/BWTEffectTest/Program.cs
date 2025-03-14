eecee787255d7ab554e6d938308a7ca2d9d7bbf6
psiblvdegod <artyombaly@list.ru>
2025-03-14 23:23:03 +0300

    feat : add project for compare LZW algorithm with and with no BWT. feat : add test for it

---
+++ LZW/BWTEffectTest/Program.cs
@@ -0,0 +1,13 @@
+﻿// <copyright file = "BWT.cs" author = "psiblvdegod" date = "2025">
+// under MIT license
+// </copyright>
+
+var input = "../../../TestData.txt";
+
+var outputWithNoBWT = LZW.LZW.Compress(input);
+
+var outputWithBWT = BWTxLZW.BWTxLZW.Compress(input);
+
+var difference = (double)outputWithNoBWT.Length / outputWithBWT.Length;
+
+Console.WriteLine($"Text with no BWT is {difference} times bigger.");
