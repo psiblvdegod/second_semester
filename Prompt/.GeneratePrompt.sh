7c83de87ae24585c9bb69487bcc3fa5b7ecd1f56
psiblvdegod <artyombaly@list.ru>
2025-03-20 18:12:35 +0300

    fix : fix GeneratePrompt to count both staged and unstaged

--- Prompt/.GeneratePrompt.sh
+++ Prompt/.GeneratePrompt.sh
@@ -12,10 +12,10 @@ end="\e[0m"
 GitStatus=$(git status --porcelain 2>/dev/null)
 
 if [ $? -eq 0 ]; then
-	modified=$(echo "$GitStatus" | grep -E '^ M' | wc -l)
-	untracked=$(echo "$GitStatus"| grep -E '^\?\?' | wc -l)
-	deleted=$(echo "$GitStatus" | grep -E '^ D' | wc -l)
-	new=$(echo "$GitStatus" | grep -E '^A' | wc -l)
+	modified=$(echo "$GitStatus" | grep -E '^\s*M' | wc -l)
+	untracked=$(echo "$GitStatus"| grep -E '^\s*\?' | wc -l)
+	deleted=$(echo "$GitStatus" | grep -E '^\s*D' | wc -l)
+	new=$(echo "$GitStatus" | grep -E '^\s*A' | wc -l)
 	prompt="${blue}git:${end} modified:$red$modified$end untracked:$red$untracked$end deleted:$red$deleted$end new:$red$new$end\n$blue>$end "
 else
 	size=$(df -h --output=size | awk 'NR==2' | tr -d ' ')
