82dad4f1b334b68d8399baa23a129486b8323134
psiblvdegod <artyombaly@list.ru>
2025-03-22 20:37:14 +0300

    feat : add another simple test for debug

--- Routers/MST.Tests/MST.Tests.cs
+++ Routers/MST.Tests/MST.Tests.cs
@@ -4,7 +4,7 @@
 public class Tests
 {
     [Test]
-    public void Build_SimpleTestWithReplacing()
+    public void Build_SimpleTest1()
     {
         var topology = "0 1 5 2 15\n1 2 10\n2 1 10";
 
@@ -12,11 +12,19 @@ public class Tests
     }
 
     [Test]
-    public void Build_BigTopology()
+    public void Build_SimpleTest2()
     {
         var topology = "0 1 5\n2 1 10\n0 2 15\n1 3 20\n3 0 25";
 
         var result = MST.Build(topology);
 
+    }
+
+    [Test]
+    public void Build_SimpleTest3()
+    {
+        var topology = "0 1 1 2 1 3 1\n2 1 2 3 2";
+
+        var result = MST.Build(topology);
     }
 }
