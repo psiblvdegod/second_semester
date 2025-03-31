4c45bf58f4ed70c1f4a7ad0a642e62fd0b90a108
psiblvdegod <artyombaly@list.ru>
2025-03-31 14:53:50 +0000

    feat : add FileProcessing.cs, add project and implementation for Program

---
+++ ParseTree/ParseTree/FileProcessing.cs
@@ -0,0 +1,41 @@
+﻿namespace ParseTree;
+
+public static class FileProcessing
+{
+    public static void Run(string path)
+    {
+        string expression;
+
+        try
+        {
+            expression = File.ReadAllText(path);
+        }
+        catch (Exception ex) when (ex is FileNotFoundException || ex is DirectoryNotFoundException) 
+        {
+            Console.WriteLine("File was not found.");
+            return;
+        }
+        catch (Exception ex) when (ex is ArgumentException || ex is ArgumentNullException)
+        {
+            Console.WriteLine("Argument \"path\" passed to method is invalid.");
+            return;
+        }
+
+        ParseTree.Tree expressionAsParseTree;
+
+        try
+        {
+            expressionAsParseTree = new ParseTree.Tree(expression);
+        }
+        catch (InvalidExpressionException ex)
+        {
+            Console.WriteLine("Expression in file is invalid.");
+            Console.WriteLine(ex.Message);
+            return;
+        }
+
+        var result = expressionAsParseTree.Calculate();
+
+        Console.WriteLine($"result: {result}");
+    }
+}
---
+++ ParseTree/Program/Program.cs
@@ -0,0 +1,13 @@
+﻿// <copyright file="Program.cs" author="psiblvdegod">
+// under MIT License.
+// </copyright>
+
+if (args.Length != 1)
+{
+    Console.WriteLine("Incorrect number of arguments passed to program.");
+    return 1;
+}
+
+ParseTree.FileProcessing.Run(args[0]);
+
+return 0;
