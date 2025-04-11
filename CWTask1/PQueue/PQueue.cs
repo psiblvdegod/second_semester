d94e2bd6bdc88cf3e9ce1d9d2027efb761acd1ce
psiblvdegod <artyombaly@list.ru>
2025-04-11 11:27:32 +0000

    feat: rewrite methods to use binary heap

--- CWTask1/PQueue/PQueue.cs
+++ CWTask1/PQueue/PQueue.cs
@@ -5,51 +5,14 @@ namespace PQueue;
 
 public class PQueue<T> : IPQueue<T>
 {
-    List<(T data, int priority)> elements = [];
+    BinaryHeap<T> elements = new();
 
     public T Dequeue()
-    {
-        var index = GetIndexOfMin();
-
-        var result = elements[index].data;
-
-        elements.RemoveAt(index);
-
-        return result;
-
-        int GetIndexOfMin()
-        {
-            int result = 0;
-
-            for (var i = 1; i < elements.Count; ++i)
-            {
-                if (elements[i].priority < elements[result].priority)
-                {
-                    result = i;
-                }
-            }
-
-            return result;
-        }
-    }
+        => elements.GetMin();
 
     public void Enqueue(T element, int priority)
-        => elements.Add((element, priority));
+       => elements.Add(element, priority);
 
     public bool IsEmpty()
-        => elements.Count == 0;
-}
-
-public class BinaryHeap<T>
-{
-    private class Node(T data, int priority)
-    {
-        public T Data { get; } = data;
-
-        public int Priority { get; } = priority;
-
-        public Node? LeftChild {get; set;} = null;
-
-        public Node? RightChild {get; set;} = null;
-    }
+        => elements.IsEmpty();
 }
