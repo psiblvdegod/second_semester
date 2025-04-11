a38ccac0cbf74cd0d0bad3341f5cd28f608cd415
psiblvdegod <artyombaly@list.ru>
2025-04-11 11:31:13 +0000

    fix: recreate project for tests

---
+++ CWTask1/PQueue.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace PQueue.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
