c5cd99f74b04a270482d3c27c144d1517520d34b
psiblvdegod <artyombaly@list.ru>
2025-04-11 12:01:42 +0000

    feat: add simple tests for IsEmpty()

---
+++ CWTask1/PQueue.Tests/Tests.cs
@@ -0,0 +1,22 @@
+﻿namespace PQueue;
+
+public class Tests
+{
+    [Test]
+    public void IsEmpty_OnEmptyQueue()
+    {
+        var queue = new PQueue<int>();
+
+        Assert.That(queue.IsEmpty());
+    }
+
+    [Test]
+    public void IsEmpty_OnNonEmptyQueue()
+    {
+        var queue = new PQueue<int>();
+
+        queue.Enqueue(2, 1);
+
+        Assert.That(queue.IsEmpty(), Is.False);
+    }
+}
