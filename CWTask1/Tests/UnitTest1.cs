67677f0f919dccf1b19d98531df79ad6ed9ff92f
psiblvdegod <artyombaly@list.ru>
2025-04-11 10:48:45 +0000

    feat: add simple test for PQueue

--- CWTask1/Tests/UnitTest1.cs
+++ CWTask1/Tests/UnitTest1.cs
@@ -1,15 +1,26 @@
-﻿namespace Tests;
+﻿namespace PQueue;
 
 public class Tests
 {
-    [SetUp]
-    public void Setup()
-    {
-    }
-
     [Test]
-    public void Test1()
+    public void qtest1()
     {
-        Assert.Pass();
+        var q = new PQueue<int>();
+
+        q.Enqueue(1, 1);
+        
+        q.Enqueue(3, 3);
+        
+        q.Enqueue(2, 2);
+
+        q.Enqueue(4, 4);
+
+        Assert.That(q.Dequeue(),  Is.EqualTo(1));
+        
+        Assert.That(q.Dequeue(),  Is.EqualTo(2));
+        
+        Assert.That(q.Dequeue(),  Is.EqualTo(3));
+        
+        Assert.That(q.Dequeue(),  Is.EqualTo(4));
     }
 }
