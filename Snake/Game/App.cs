52382a9101844d06635b7401c7c3bc46dd3bdc1a
psiblvdegod <artyombaly@list.ru>
2025-04-12 00:01:26 +0300

    feat: add class App : Avalonia.Application

---
+++ Snake/Game/App.cs
@@ -0,0 +1,36 @@
+using Avalonia.Controls;
+using Avalonia.Controls.ApplicationLifetimes;
+using Tmds.DBus.Protocol;
+
+namespace Game;
+
+public class App : Avalonia.Application
+{
+    private MainWindow Window = new();
+
+    private IClassicDesktopStyleApplicationLifetime? Desktop;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+    }
+
+    public override void OnFrameworkInitializationCompleted()
+    {
+        ConfigureDesktop();
+        base.OnFrameworkInitializationCompleted();
+    }
+
+    private void ConfigureDesktop()
+    {
+        if (this.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+        {
+            desktop.MainWindow = this.Window;
+            this.Desktop = desktop;
+        }
+        else
+        {
+            throw new NotSupportedException("seems like your platform is unsupported.");
+        }
+    }
+}
