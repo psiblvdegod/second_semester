9f710a7f6f79a3970b32a8380851c3cccb90bd03
psiblvdegod <artyombaly@list.ru>
2025-04-12 03:15:42 +0300

    feat: add handler to buttons

--- Snake/Game/Game.cs
+++ Snake/Game/Game.cs
@@ -1,4 +1,6 @@
 using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
 using static Game.UIInitialization;
 
 namespace Game;
@@ -41,5 +43,73 @@ public class Game : IMove
         Window.SetCell(Position.x, Position.y, CreateSpace());
         ++Position.x;
         Window.SetCell(Position.x, Position.y, character);
+    }
+
+    public void KeyHandler(object? sender, KeyEventArgs args)
+    {
+        var key = args.Key;
+
+        if (key == Key.W)
+        {
+            this.MoveUp();
+        }
+        else if (key == Key.A)
+        {
+            this.MoveLeft();
+        }
+        else if (key == Key.S)
+        {
+            this.MoveDown();
+        }
+        else if (key == Key.D)
+        {
+            this.MoveRight();
+        }
+    }
+
+    public void SubToButtons()
+    {
+        if (Window.Content is Panel panel)
+        {
+            foreach (var child in panel.Children)
+            {
+                if (child is StackPanel buttons)
+                {
+                    foreach (var subchild in buttons.Children)
+                    {
+                        if (subchild is Button button)
+                        {
+                            button.Click += ButtonHandler;
+                            button.IsEnabled = true;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public void ButtonHandler(object? sender, RoutedEventArgs args)
+    {
+        if (sender is Button button)
+        {
+            if (button.Content is char c)
+            {
+                switch (c)
+                {
+                    case 'W':
+                        this.MoveUp();
+                    break;
+                    case 'A':
+                        this.MoveLeft();
+                    break;
+                    case 'S':
+                        this.MoveDown();
+                    break;
+                    case 'D':
+                        this.MoveRight();
+                    break;
+                }
+            }
+        }
     }
 }
