60cac386a0339bbe5817a8903a87b7d1b0c68608
psiblvdegod <artyombaly@list.ru>
2025-04-21 20:13:21 +0300

    feat: add project for tests for calculator and some simple tests for it

---
+++ GUI-Calculator/Calculator.Tests/Tests.cs
@@ -0,0 +1,54 @@
+﻿namespace Calculator.Tests;
+
+public class Tests
+{
+    Calculator calculator;
+
+    [SetUp]
+    public void Setup()
+    {
+        calculator = new();
+    }
+
+    [Test]
+    public void Test1()
+    {
+        calculator.AddToken('1');
+        calculator.AddToken('2');
+        calculator.AddToken('+');
+        calculator.AddToken('3');
+        calculator.AddToken('-');
+        Assert.That(calculator.Value, Is.EqualTo(15.0).Within(0.001));
+    }
+
+    [Test]
+    public void Test2()
+    {
+        calculator.AddToken('1');
+        calculator.AddToken('*');
+        calculator.AddToken('2');
+        calculator.AddToken('3');
+        calculator.AddToken('-');
+        Assert.That(calculator.Value, Is.EqualTo(23.0).Within(0.001));
+    }
+
+    [Test]
+    public void Test3()
+    {
+        calculator.AddToken('1');
+        calculator.AddToken('0');
+        calculator.AddToken('0');
+        calculator.AddToken('0');
+        calculator.AddToken('-');
+        calculator.AddToken('9');
+        calculator.AddToken('9');
+        calculator.AddToken('9');
+        calculator.AddToken('-');
+        calculator.AddToken('1');
+        calculator.AddToken('+');
+        calculator.AddToken('5');
+        calculator.AddToken('0');
+        calculator.AddToken('-');
+        Assert.That(calculator.Value, Is.EqualTo(50.0).Within(0.001));
+    }
+}
