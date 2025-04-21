e7a34541cf6d4895580ade2378c9514538f2f867
psiblvdegod <artyombaly@list.ru>
2025-04-21 21:35:46 +0300

    feat: add class App

---
+++ GUI-Calculator/GUI/App.cs
@@ -0,0 +1,26 @@
+﻿using Avalonia.Controls.ApplicationLifetimes;
+
+namespace GUI;
+
+public class App : Avalonia.Application
+{
+    private MainWindow? Window;
+
+    public override void OnFrameworkInitializationCompleted()
+    {
+        this.Window = new MainWindow();
+        ConfigureDesktop();
+    }
+
+    private void ConfigureDesktop()
+    {
+        if (this.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+        {
+            desktop.MainWindow = this.Window;
+        }
+        else
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
