cac1d1eb00ab2c091f689a9648b36dfb3123eb2d
psiblvdegod <artyombaly@list.ru>
2025-04-21 20:01:34 +0300

    feat: add class calculator and AddToken() method

---
+++ GUI-Calculator/Calculator/Calculator.cs
@@ -0,0 +1,56 @@
+﻿using System.Data;
+using Microsoft.VisualBasic;
+
+namespace Calculator;
+
+public class Calculator
+{
+    public static string Digits { get; } = "0123456789";
+
+    public void AddToken(char token)
+    {
+        if (isStart)
+        {
+            if (Digits.Contains(token))
+            {
+                operandBuffer += token;
+            }
+            else if (ValidOperations.ContainsKey(token))
+            {
+                Value = double.Parse(operandBuffer);
+                operationBuffer = token;
+                operandBuffer = string.Empty;
+                isStart = false;
+            }
+        }
+        else
+        {
+            if (Digits.Contains(token))
+            {
+                operandBuffer += token;
+            }
+            else if (ValidOperations.ContainsKey(token))
+            {
+                Value = ValidOperations[operationBuffer](Value, double.Parse(operandBuffer));
+                operationBuffer = token;
+                operandBuffer = string.Empty;
+            }
+        }
+    }
+
+    private bool isStart = true;
+
+    public double Value { get; private set; }
+    
+    private char operationBuffer;
+
+    private string operandBuffer = string.Empty;
+
+    public static readonly Dictionary<char, Func<double, double, double>> ValidOperations = new()
+    {
+        ['+'] = (x,y) => x + y,
+        ['*'] = (x,y) => x * y,
+        ['-'] = (x,y) => x - y,
+        ['/'] = (x,y) => x / y,
+    };
+}
