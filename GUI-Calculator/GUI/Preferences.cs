1ff894bf9039de60ba41ea8fe5144507b547192a
psiblvdegod <artyombaly@list.ru>
2025-04-21 21:36:47 +0300

    feat: add class Preferences

---
+++ GUI-Calculator/GUI/Preferences.cs
@@ -0,0 +1,12 @@
+using Avalonia.Media;
+
+namespace GUI;
+
+public static class Preferences
+{
+    public static int GridWidth { get; } = 4;
+
+    public static int GridHeight { get; } = 5;
+
+    public static int CellSize { get; } = 30;
+}
