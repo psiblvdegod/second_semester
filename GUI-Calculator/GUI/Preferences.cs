bd3c133e3dd6eff5ffc27a05563353a5ee6214e4
psiblvdegod <artyombaly@list.ru>
2025-04-24 23:35:34 +0300

    minor: change grid size to add new buttons

--- GUI-Calculator/GUI/Preferences.cs
+++ GUI-Calculator/GUI/Preferences.cs
@@ -4,7 +4,7 @@ public static class Preferences
 {
     public static int GridWidth { get; } = 4;
 
-    public static int GridHeight { get; } = 5;
+    public static int GridHeight { get; } = 6;
 
     public static int CellSize { get; } = 100;
 
