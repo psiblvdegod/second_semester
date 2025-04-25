a18786d3bb1b0111584c8a2a188c8c08ed0436ae
psiblvdegod <artyombaly@list.ru>
2025-04-25 21:08:27 +0300

    fix code style

--- GUI-Calculator/GUI/Preferences.cs
+++ GUI-Calculator/GUI/Preferences.cs
@@ -1,5 +1,15 @@
+// <copyright file="Preferences.cs" author="psiblvdegod">
+// under MIT License
+// </copyright>
+
 namespace GUI;
 
+// SA160: Elements should be documented.
+#pragma warning disable SA1600
+
+/// <summary>
+/// Stores preferences for application.
+/// </summary>
 public static class Preferences
 {
     public static int GridWidth { get; } = 4;
@@ -8,7 +18,7 @@ public static class Preferences
 
     public static int CellSize { get; } = 100;
 
-    public static int WindowWidth { get; } = GridWidth * CellSize + CellSize / 5;
+    public static int WindowWidth { get; } = (GridWidth * CellSize) + (CellSize / 5);
 
-    public static int WindowHeight { get; } = GridHeight * CellSize + CellSize / 5;
+    public static int WindowHeight { get; } = (GridHeight * CellSize) + (CellSize / 5);
 }
--- GUI-Calculator/Program/Program.cs
+++ GUI-Calculator/Program/Program.cs
@@ -1,6 +1,11 @@
-﻿using Avalonia;
-using GUI;
+﻿// <copyright file="Program.cs" author="psiblvdegod">
+// under MIT License
+// </copyright>
 
-AppBuilder.Configure<App>()
+#pragma warning disable SA1200
+
+using Avalonia;
+
+AppBuilder.Configure<GUI.App>()
     .UsePlatformDetect()
     .StartWithClassicDesktopLifetime(args);
