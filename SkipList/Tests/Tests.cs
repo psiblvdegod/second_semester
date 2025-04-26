922a9dab8a0e2e1a7731af488cf7aaeed120c124
psiblvdegod <artyombaly@list.ru>
2025-04-26 22:03:35 +0300

    feat: add simple test for SKip List

---
+++ SkipList/Tests/Tests.cs
@@ -0,0 +1,32 @@
+﻿namespace Tests;
+
+using SkipList;
+
+public class Tests
+{
+    private SkipList<int> list; 
+
+    [SetUp]
+    public void Setup()
+    {
+        list = new();
+    }
+
+    [Test]
+    public void Test1()
+    {
+        int[] data = [1, 10, -20, 5, 3, 2, 0, 1, 2, 3, 6];
+
+        foreach (var i in data)
+        {
+            list.Add(i);
+        }
+        
+        foreach (var i in data)
+        {
+            Assert.That(list.Contains(i));
+        }
+
+        Assert.That(list.Contains(4), Is.False);
+    }
+}
