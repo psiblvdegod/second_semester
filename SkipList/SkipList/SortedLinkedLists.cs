c2d5726f88919f36984603d5224d27fe728b8bba
psiblvdegod <artyombaly@list.ru>
2025-04-27 01:54:35 +0300

    feat: add FindBigger()

--- SkipList/SkipList/SortedLinkedLists.cs
+++ SkipList/SkipList/SortedLinkedLists.cs
@@ -29,4 +29,19 @@ public class SortedLinkLists<T> where T : IComparable
             }
         }
     }
+
+    public Node<T>? FindBigger(T item)
+    {
+        return RecCall(Head);
+
+        Node<T>? RecCall(Node<T>? current)
+        {
+            if (current is null || item.CompareTo(current.Item) < 0)
+            {
+                return current;
+            }
+
+            return RecCall(current.Next);
+        }
+    }
 }
