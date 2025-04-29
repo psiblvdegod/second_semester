7c519196d3f44d22b576f81e515920dd592352c4
psiblvdegod <@>
2025-04-29 20:57:16 +0300

    feat: add some tests

--- SkipList/Tests/Tests.cs
+++ SkipList/Tests/Tests.cs
@@ -4,16 +4,11 @@ using SkipList;
 
 public class Tests
 {
-    SkipLists<int> list;
-    [SetUp]
-    public void Setup()
-    {
-        list = new ();
-    }
-
     [Test]
     public void Contains_OnEmptyList_ShouldReturnFalse()
     {
+        SkipLists<int> list = new ();
+
         for (var i = 1; i < 100; ++i)
         {
             Assert.That(list.Contains(i), Is.False);
@@ -21,13 +16,15 @@ public class Tests
     }
 
     [Test]
-    public void Add_OnRandomPositiveNumbers()
+    public void Add_OnRandom()
     {
-        var data = new int[50];
+        SkipLists<int> list = new ();
+
+        var data = new int[10000];
         var rand = new Random();
         for (var i = 0; i < data.Length; ++i)
         {
-            data[i] = Math.Abs(rand.Next() % 100) + 1;
+            data[i] = Math.Abs(rand.Next() % 1000 - 500) + 1;
         }
         foreach (var i in data)
         {
@@ -42,6 +39,8 @@ public class Tests
     [Test]
     public void Add_OnRandomNumbers()
     {
+        SkipLists<int> list = new ();
+
         var data = new int[10000];
 
         var rand = new Random();
@@ -61,4 +60,41 @@ public class Tests
             Assert.That(list.Contains(i));
         }
     }
+
+    [Test]
+    public void Add_OnString()
+    {
+        SkipLists<string> list = new();
+
+        var rand = new Random();
+
+        var data = new string[1000];
+
+        for (var i = 0; i < data.Length; ++i)
+        {
+            data[i] = rand.Next().ToString();
+        }
+
+        foreach (var s in data)
+        {
+            list.Add(s);
+        }
+
+        foreach (var s in data)
+        {
+            Assert.That(list.Contains(s));
+        }
+    }
+
+    /*
+    [Test]
+    public void Contains_OnZero()
+    {
+        Assert.That(list.Contains(0), Is.False);
+
+        list.Add(0);
+
+        Assert.That(list.Contains(0));
+    }
+    */
 }
