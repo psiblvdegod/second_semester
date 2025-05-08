4fb9643a276574c9226345ca223adbffa40c169e
psiblvdegod <@>
2025-05-08 22:15:33 +0300

    assert that compression does not corrupt data in CompressionMeasuring

--- LZW/CompressionMeasuring/Program.cs
+++ LZW/CompressionMeasuring/Program.cs
@@ -6,10 +6,10 @@
 /// measures the effect of BWT on compression ratio.
 /// </summary>
 
-var pathForFirstMeasurement = "../DataForTests/TestData.txt";
+var pathForFirstMeasurement = "../DataForTests/BigTextForTest.txt";
 var resultOfFirstMeasurement = MeasureOnText(pathForFirstMeasurement);
 
-var pathForSecondMeasurement = "../DataForTests/TestData";
+var pathForSecondMeasurement = "../DataForTests/BigBinaryForTest";
 var resultOfSecondMeasurement = MeasureOnBinary(pathForSecondMeasurement);
 
 Console.WriteLine($"first measure: text is {resultOfFirstMeasurement} times bigger with no BWT.");
@@ -20,6 +20,16 @@ static double MeasureOnText(string path)
     var input = File.ReadAllText(path);
     var outputWithNoBWT = LZW.Compression.Compress(input);
     var outputWithBWT = LZW.CompressionWithBWT.Compress(input);
+
+    if (LZW.Compression.Decompress(outputWithNoBWT) != input)
+    {
+        throw new Exception("Compression corrupted the data.");
+    }
+    if (LZW.CompressionWithBWT.Decompress(outputWithBWT) != input)
+    {
+        throw new Exception("Compression with BWT corrupted the data.");
+    }
+
     return (double)outputWithNoBWT.Length / outputWithBWT.Length;
 }
 
@@ -28,5 +38,15 @@ static double MeasureOnBinary(string path)
     var input = File.ReadAllBytes(path);
     var outputWithNoBWT = LZW.Compression.Compress(input);
     var outputWithBWT = LZW.CompressionWithBWT.Compress(input);
+
+    if (!LZW.Compression.Decompress(outputWithNoBWT).SequenceEqual(input))
+    {
+        throw new Exception("Compression corrupted the data.");
+    }
+    if (!LZW.CompressionWithBWT.Decompress(outputWithBWT).SequenceEqual(input))
+    {
+        throw new Exception("Compression with BWT corrupted the data.");
+    }
+
     return (double)outputWithNoBWT.Length / outputWithBWT.Length;
 }
