4a075158688ff8fd1c88239789a6a11a7589a889
psiblvdegod <@>
2025-05-12 12:28:16 +0300

    fix endless loop bug

--- MyLinq/MyLinq/IEnumerableExtensions.cs
+++ MyLinq/MyLinq/IEnumerableExtensions.cs
@@ -6,9 +6,6 @@ public static class IEnumerableExtentions
 {
     public static IEnumerable<T> MyTake<T>(this IEnumerable<T> source, int count)
     {
-        ArgumentOutOfRangeException.ThrowIfGreaterThanOrEqual(count, source.Count());
-        ArgumentOutOfRangeException.ThrowIfLessThan(count, 1);
-
         IEnumerator<T> enumerator = source.GetEnumerator();
 
         for (var counter = 0; counter < count && enumerator.MoveNext(); ++counter)
@@ -19,9 +16,6 @@ public static class IEnumerableExtentions
 
     public static IEnumerable<T> MySkip<T>(this IEnumerable<T> source, int count)
     {
-        ArgumentOutOfRangeException.ThrowIfGreaterThanOrEqual(count, source.Count());
-        ArgumentOutOfRangeException.ThrowIfLessThan(count, 1);
-
         IEnumerator<T> enumerator = source.GetEnumerator();
 
         for (var i = 0; i < count; ++i)
