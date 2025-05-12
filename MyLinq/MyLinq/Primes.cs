f44f961c9f6c7db76e1f62ddf88a69cdb8f5bd4d
psiblvdegod <@>
2025-05-12 12:27:49 +0300

    add Primes.Get()

---
+++ MyLinq/MyLinq/Primes.cs
@@ -0,0 +1,36 @@
+﻿namespace MyLinq;
+
+public static class Primes
+{
+    public static IEnumerable<int> Get()
+    {
+        for (var current = 1; ; current = GetNextPrime(current))
+        {
+            yield return current;
+        }
+    }
+
+    private static int GetNextPrime(int current)
+    {
+        while (true)
+        {
+            ++current;
+
+            var isPrime = true;
+
+            for (var divider = 2; divider <= (int)Math.Sqrt(current); ++divider)
+            {
+                if (current % divider == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime)
+            {
+                return current;
+            }
+        }
+    }
+}
--- MyLinq/Tests/Tests.cs
+++ MyLinq/Tests/Tests.cs
@@ -4,14 +4,14 @@ using MyLinq;
 
 public class Tests
 {
-    private static readonly IEnumerable<int> IntSource = [ 5, 4, 3, 2, 1 ];
+    private static readonly IEnumerable<int> IntSource = [ 1, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29 ];
 
     private static readonly IEnumerable<string> StringSource = [ "2", "4", "6", "1", "3", "5" ];
 
     [Test]
     public void MyTake_OnInts()
     {
-        int count = 3;
+        var count = 3;
         var expectedResult = IntSource.Take(count);
         var actualResult = IntSource.MyTake(count);
         Assert.That(actualResult.SequenceEqual(expectedResult));
@@ -20,43 +20,68 @@ public class Tests
     [Test]
     public void MySkip_OnStrings()
     {
-        int count = 1;
+        var count = 5;
+        var expectedResult = StringSource.Skip(count);
+        var actualResult = StringSource.MySkip(count);
+        Assert.That(actualResult.SequenceEqual(expectedResult));
+    }
+
+    [Test]
+    public void MySkip_OnNonPozitiveCount_ShouldReturnInitialSequence()
+    {
+        var count = -5;
         var expectedResult = IntSource.Skip(count);
+        Assert.That(expectedResult.SequenceEqual(IntSource));
         var actualResult = IntSource.MySkip(count);
         Assert.That(actualResult.SequenceEqual(expectedResult));
     }
 
     [Test]
-    public void MyTake_Throws_WhenCountIsNegative()
-        => Assert.Throws<ArgumentOutOfRangeException>(() => StringSource.MyTake(-5).First());
-    
-    [Test]
-    public void MySkip_Throws_WhenCountIsNegative()
-        => Assert.Throws<ArgumentOutOfRangeException>(() => IntSource.MySkip(-5).First());
-
-    [Test]
-    public void MyTake_Throws_WhenCountIsToBig()
-        => Assert.Throws<ArgumentOutOfRangeException>(() => IntSource.MyTake(IntSource.Count()).First());
-    
-    [Test]
-    public void MySkip_Throws_WhenCountIsToBig()
-        => Assert.Throws<ArgumentOutOfRangeException>(() => StringSource.MySkip(StringSource.Count() + 1).First());
-
-    [Test]
-    public void MyTake_ShouldBeLazy()
+    public void MyTake_OnNonPozitiveCount_ShouldReturnEmptySequence()
     {
-        IEnumerable<int>? incorrertSequence = null;
-        Assert.DoesNotThrow(() => incorrertSequence = IntSource.MyTake(-1));
-        Assert.That(incorrertSequence, Is.Not.Null);
-        Assert.Throws<ArgumentOutOfRangeException>(() => incorrertSequence.First());
+        var count = 0;
+        var expectedResult = StringSource.Take(count);
+        Assert.That(expectedResult.SequenceEqual([]));
+        var actualResult = StringSource.MyTake(count);
+        Assert.That(actualResult.SequenceEqual(expectedResult));
     }
 
     [Test]
-    public void MySkip_ShouldBeLazy()
+    public void MyTake_OnIndexOutOfRange_ShouldReturnInitialSequence()
     {
-        IEnumerable<int>? incorrertSequence = null;
-        Assert.DoesNotThrow(() => incorrertSequence = IntSource.MySkip(-1));
-        Assert.That(incorrertSequence, Is.Not.Null);
-        Assert.Throws<ArgumentOutOfRangeException>(() => incorrertSequence.First());
+        var count = IntSource.Count() + 1;
+        var expectedResult = IntSource.Take(count);
+        Assert.That(expectedResult.SequenceEqual(IntSource));
+        var actualResult = IntSource.MyTake(count);
+        Assert.That(actualResult.SequenceEqual(expectedResult));
+    }
+
+    [Test]
+    public void MySkip_OnIndexOutOfRange_ShouldReturnEmptySequence()
+    {
+        var count = StringSource.Count() + 1;
+        var expectedResult = StringSource.Skip(count);
+        Assert.That(expectedResult.SequenceEqual([]));
+        var actualResult = StringSource.MySkip(count);
+        Assert.That(actualResult.SequenceEqual(expectedResult));
+    }
+
+    [Test]
+    public void Primes_Get_WithMyTake()
+    {
+        var count = 7;
+        var expectedResult = IntSource.Take(count);
+        var actualResult = Primes.Get().MyTake(count);
+        Assert.That(actualResult.SequenceEqual(expectedResult));
+    }
+
+    [Test]
+    public void Primes_Get_WithMySkipThenMyTake()
+    {
+        var skipCount = 4;
+        var takeCount = 3;
+        var expectedResult = IntSource.Skip(skipCount).Take(takeCount);
+        var actualResult = Primes.Get().MySkip(skipCount).MyTake(takeCount);
+        Assert.That(actualResult.SequenceEqual(expectedResult));
     }
 }
