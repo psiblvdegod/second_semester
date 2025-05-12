d7e4621d81b600c0b19dfd0d0f860e3d31494032
psiblvdegod <@>
2025-05-12 14:58:34 +0300

    add some tests and header

--- MyLinq/Tests/Tests.cs
+++ MyLinq/Tests/Tests.cs
@@ -1,12 +1,18 @@
-﻿namespace Tests;
+﻿// <copyright file="Tests.cs" company="_">
+// psiblvdegod, 2025, under MIT License.
+// </copyright>
+
+namespace Tests;
 
 using MyLinq;
 
+#pragma warning disable SA1600
+
 public class Tests
 {
-    private static readonly IEnumerable<int> IntSource = [ 1, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29 ];
+    private static readonly IEnumerable<int> IntSource = [1, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29];
 
-    private static readonly IEnumerable<string> StringSource = [ "2", "4", "6", "1", "3", "5" ];
+    private static readonly IEnumerable<string> StringSource = ["2", "4", "6", "1", "3", "5"];
 
     [Test]
     public void MyTake_OnInts()
