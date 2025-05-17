ab4fe6d6d07c5c773320bb4a513aea07a15950d8
psiblvdegod <artyombaly@list.ru>
2025-05-18 02:50:55 +0300

    delete useless error handlers

--- ParseTree/Program/Program.cs
+++ ParseTree/Program/Program.cs
@@ -1,47 +1,18 @@
-﻿// <copyright file="Program.cs" author="psiblvdegod">
-// under MIT License.
+﻿// <copyright file="Program.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
 // </copyright>
 
 using ParseTree;
 
 if (args.Length != 1)
 {
-    Console.WriteLine("Incorrect number of arguments passed to program.");
-    return 1;
+    throw new ArgumentException("Incorrect number of arguments passed to program.");
 }
 
-string expression;
+string expression = File.ReadAllText(args[0]);
 
-try
-{
-    expression = File.ReadAllText(args[0]);
-}
-catch (Exception ex) when (ex is ArgumentException || ex is ArgumentNullException)
-{
-    Console.WriteLine("Argument \"path\" passed to method is invalid.");
-    return 2;
-}
-catch (Exception ex) when (ex is FileNotFoundException || ex is DirectoryNotFoundException)
-{
-    Console.WriteLine("File was not found.");
-    return 3;
-}
-
-Tree expressionAsParseTree;
-
-try
-{
-    expressionAsParseTree = new Tree(expression);
-}
-catch (InvalidExpressionException ex)
-{
-    Console.WriteLine("Expression in file is invalid.");
-    Console.WriteLine(ex.Message);
-    return 4;
-}
+var expressionAsParseTree = new Tree(expression);
 
 var result = expressionAsParseTree.Calculate();
 
 Console.WriteLine($"result: {result}");
-
-return 0;
