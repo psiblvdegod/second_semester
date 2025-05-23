3c2edc037694fa7a00ddf584018ce0e141e025bd
psiblvdegod <artyombaly@list.ru>
2025-05-23 15:01:12 +0300

    add test for null counter

--- ZeroElements/Tests/Tests.cs
+++ ZeroElements/Tests/Tests.cs
@@ -1,5 +1,5 @@
-﻿// <copyright file="Tests.cs" company="PlaceholderCompany">
-// Copyright (c) PlaceholderCompany. All rights reserved.
+﻿// <copyright file="Tests.cs" company="_">
+// psiblvdegod, 2025, under MIT License.
 // </copyright>
 namespace Tests;
 
@@ -11,10 +11,10 @@ public class Tests
 {
     private static readonly int[] IntItems = [1, 0, 2, 0, 3, 0, 4, 0];
 
-    private static readonly string[] StringItems;
+    private static readonly string[] StringItems = [string.Empty, "123", "100", string.Empty, string.Empty, "A"];
 
     [Test]
-    public void Test1()
+    public void CountNulls_OnInts()
     {
         var list = new MyList.List<int>();
 
@@ -26,7 +26,24 @@ public class Tests
         Predicate<int> isNull = x => x == 0;
 
         var expectedResult = 4;
+        var actualResult = list.CountNulls(isNull);
 
+        Assert.That(actualResult, Is.EqualTo(expectedResult));
+    }
+
+    [Test]
+    public void CountNulls_OnString()
+    {
+        var list = new MyList.List<string>();
+
+        foreach (var item in StringItems)
+        {
+            list.Add(item);
+        }
+
+        Predicate<string> isNull = x => x == string.Empty;
+
+        var expectedResult = 3;
         var actualResult = list.CountNulls(isNull);
 
         Assert.That(actualResult, Is.EqualTo(expectedResult));
