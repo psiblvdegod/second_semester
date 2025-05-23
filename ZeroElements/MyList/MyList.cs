2acd4a82685680d1bc40bce108fd9ec2024e2943
psiblvdegod <artyombaly@list.ru>
2025-05-23 14:16:49 +0300

    add Add(), Enumerator and simple test

---
+++ ZeroElements/MyList/MyList.cs
@@ -0,0 +1,58 @@
+﻿namespace MyList;
+
+public class List<T>
+{
+    private static readonly int InitialLenght = 10;
+    private T[] items = new T[InitialLenght];
+    public int Count { get; private set; } = 0;
+    public int Capacity { get; private set; } = InitialLenght;
+
+    public void Add(T item)
+    {
+        ++this.Count;
+        UpdateSize();
+        this.items[this.Count] = item;
+
+        void UpdateSize()
+        {
+            if (this.Count == this.Capacity)
+            {
+                this.Capacity *= 2;
+                var newItems = new T[this.Capacity];
+                for (var i = 0; i < this.Count; ++i)
+                {
+                    newItems[i] = this.items[i];
+                }
+                this.items = newItems;
+            }
+        }
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        return new Enumerator(items);
+    }
+
+    private class Enumerator(T[] items) : IEnumerator<T>
+    {
+        private T[] items = items;
+
+        int index = 0;
+
+        public T Current
+            => items[this.index];
+
+        object System.Collections.IEnumerator.Current => this.Current;
+
+        public bool MoveNext()
+            => ++this.index < items.Length;
+
+        public void Dispose()
+        {
+        }
+
+        public void Reset()
+        {
+        }
+    }
+}
---
+++ ZeroElements/Tests/Tests.cs
@@ -0,0 +1,25 @@
+﻿namespace Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        var list = new MyList.List<int>();
+
+        for (var i = 0; i < 15; ++i)
+        {
+            list.Add(i);
+        }
+
+        foreach (var i in list)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
