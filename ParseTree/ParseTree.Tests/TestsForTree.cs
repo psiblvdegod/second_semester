bf44e246ec21803920d332445fc20f0c11a9ae34
psiblvdegod <artyombaly@list.ru>
2025-05-29 16:11:04 +0300

    fix tests according to changes and add new ones

--- ParseTree/ParseTree.Tests/TestsForTree.cs
+++ ParseTree/ParseTree.Tests/TestsForTree.cs
@@ -11,12 +11,18 @@ namespace ParseTree.Tests;
 /// </summary>
 public class TestsForTree
 {
+    private ParseTree.Tree tree;
+
+    [SetUp]
+    public void SetUp()
+        => this.tree = new();
+
     [Test]
     public void Constructor_OnNonExistentOperation_ShouldThrowException()
     {
         var expression = "- 2 ^ 5 2";
 
-        Assert.Throws<NotSupportedException>(() => new Tree(expression));
+        Assert.Throws<NotSupportedException>(() => this.tree.Parse(expression));
     }
 
     [Test]
@@ -24,17 +30,18 @@ public class TestsForTree
     {
         var expression = "+ 2 3 4";
 
-        Assert.Throws<InvalidExpressionException>(() => new Tree(expression));
+        Assert.Throws<InvalidExpressionException>(() => this.tree.Parse(expression));
     }
 
     [Test]
     public void Calculate_OnSimpleExpression()
     {
         var expression = "+ * 1 2 5";
+        this.tree.Parse(expression);
 
         var expectedResult = 7;
 
-        var actualResult = new Tree(expression).Calculate();
+        var actualResult = this.tree.Calculate();
 
         Assert.That(actualResult, Is.EqualTo(expectedResult));
     }
@@ -43,51 +50,62 @@ public class TestsForTree
     public void Calculate_OnExpressionWithNegativeNumbers()
     {
         var expression = "+ * 1 -2 5";
+        this.tree.Parse(expression);
 
         var expectedResult = 3;
 
-        var actualResult = new Tree(expression).Calculate();
+        var actualResult = this.tree.Calculate();
 
         Assert.That(actualResult, Is.EqualTo(expectedResult));
     }
 
     [Test]
-    public void AddOperationToSupportedOnes_OnPow()
+    public void RegisterOperation_OnPow()
     {
         Func<int, int, int> operation = (x, y) => (int)Math.Pow(x, y);
-
         var token = "pow";
-
-        Tree.AddOperationToSupportedOnes(token, operation);
+        this.tree.RegisterOperation(token, operation);
 
         var expression = $"- {token} 2 5 2";
+        this.tree.Parse(expression);
 
         var expectedResult = 30;
 
-        var actualResult = new Tree(expression).Calculate();
+        var actualResult = this.tree.Calculate();
 
         Assert.That(actualResult, Is.EqualTo(expectedResult));
     }
 
     [Test]
-    public void AddOperationToSupportedOnes_OnConcat()
+    public void RegisterOperation_OnConcat()
     {
         Func<int, int, int> operation = (x, y) => int.Parse($"{x}{y}");
-
         var token = "concat";
-
-        Tree.AddOperationToSupportedOnes(token, operation);
+        this.tree.RegisterOperation(token, operation);
 
         var expression = $"+ {token} 1 2 {token} 3 4";
+        this.tree.Parse(expression);
 
         var expectedResult = 46;
 
-        var actualResult = new Tree(expression).Calculate();
+        var actualResult = this.tree.Calculate();
 
         Assert.That(actualResult, Is.EqualTo(expectedResult));
     }
 
     [Test]
-    public void AddOperationToSupportedOnes_OnExistingOperation_ShouldThrowException()
-        => Assert.Throws<InvalidOperationException>(() => Tree.AddOperationToSupportedOnes("+", (x, y) => x + y));
+    public void TryGetRegisteredOperation_OnExistingOperator()
+        => Assert.That(this.tree.TryGetRegisteredOperation("+", out _), Is.True);
+
+    [Test]
+    public void TryGetRegisteredOperation_OnNonExistingOperator()
+        => Assert.That(this.tree.TryGetRegisteredOperation("_", out _), Is.False);
+
+    [Test]
+    public void RegisterOperation_Throws_OnExistingOperation()
+        => Assert.Throws<InvalidOperationException>(() => this.tree.RegisterOperation("+", (x, y) => x + y));
+
+    [Test]
+    public void Calculate_Throws_OnEmptyTree()
+        => Assert.Throws<InvalidOperationException>(() => this.tree.Calculate());
 }
