b6e69208900036a777efb0ba2cb125e48307b18f
psiblvdegod <artyombaly@list.ru>
2025-05-29 14:22:36 +0300

    update comments according to changes

--- MapFilterFold/Functions/Functions.cs
+++ MapFilterFold/Functions/Functions.cs
@@ -13,9 +13,9 @@ public static class Functions
     /// Creates sequence applying passed function to each element in the initial sequence.
     /// </summary>
     /// <typeparam name="TSource">Type of items in the initial sequence.</typeparam>
-    /// <typeparam name="TResult">Type to which items of the initial sequence map. .</typeparam>
-    /// <param name="source">Sequence which is used to create transformed one.</param>
-    /// <param name="func">Function which will be applied to each element of the sequence.</param>
+    /// <typeparam name="TResult">Type to which items of the initial sequence map.</typeparam>
+    /// <param name="source">Sequence which is used to create new one.</param>
+    /// <param name="func">Function which will be applied to each item of the initial sequence to get new one.</param>
     /// <returns>Transformed sequence.</returns>
     public static IEnumerable<TResult> Map<TSource, TResult>(IEnumerable<TSource> source, Func<TSource, TResult> func)
     {
@@ -30,11 +30,11 @@ public static class Functions
     }
 
     /// <summary>
-    /// Creates sequence removing elements from initial sequence for which passed predicate is true.
+    /// Creates sequence removing items from initial sequence for which passed predicate is false.
     /// </summary>
-    /// <typeparam name="T">Type of elements in the sequence.</typeparam>
+    /// <typeparam name="T">Type of items in the sequence.</typeparam>
     /// <param name="source">Sequence which is used to create transformed one.</param>
-    /// <param name="predicate">Predicate which determines elements to be deleted.</param>
+    /// <param name="predicate">Predicate which determines elements to be added to new sequence.</param>
     /// <returns>Transformed sequence.</returns>
     public static IEnumerable<T> Filter<T>(IEnumerable<T> source, Predicate<T> predicate)
     {
@@ -52,15 +52,16 @@ public static class Functions
     }
 
     /// <summary>
-    /// Calculates result of doing sequence traversal and applying passed function to each element.
+    /// Accumulates and returns result of doing sequence traversal and applying passed function to each item.
+    /// Accumulated value is passed as left operand, current item of sequence as right one.
     /// </summary>
-    /// <typeparam name="TSource">Type of items in the sequence.</typeparam>
-    /// <typeparam name="TResult">Type of returned value.</typeparam>
-    /// <param name="source">Sequence which is used to calculate.</param>
-    /// <param name="initialValue">Value which is passed to function as left operand at the first call.</param>
-    /// <param name="func">Function which is used to calculate.</param>
-    /// <returns>Result of doing traversal.</returns>
-    public static TResult Fold<TSource, TResult>(IEnumerable<TSource> source, TResult initialValue, Func<TResult, TSource, TResult> func)
+    /// <typeparam name="TSource">Type of the items in the sequence.</typeparam>
+    /// <typeparam name="TValue">Type of the accumulating value.</typeparam>
+    /// <param name="initialValue">Value which is passed to the func as left operand at the first call.</param>
+    /// <param name="source">Sequence which items is used as right opperands.</param>
+    /// <param name="func">Function which is applied to accumulated value and item of the sequence.</param>
+    /// <returns>Accumulated value.</returns>
+    public static TValue Fold<TSource, TValue>(TValue initialValue, IEnumerable<TSource> source, Func<TValue, TSource, TValue> func)
     {
         var result = initialValue;
 
