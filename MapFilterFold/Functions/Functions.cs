372db7d9f9b6c9a9f8f4d446402f520af6b2795e
psiblvdegod <artyombaly@list.ru>
2025-05-29 12:40:51 +0300

    change generic types according to pr comments

--- MapFilterFold/Functions/Functions.cs
+++ MapFilterFold/Functions/Functions.cs
@@ -12,57 +12,59 @@ public static class Functions
     /// <summary>
     /// Creates sequence applying passed function to each element in the initial sequence.
     /// </summary>
-    /// <typeparam name="T">Type of elements in sequence.</typeparam>
-    /// <param name="elements">Sequence which is used to create transformed one.</param>
+    /// <typeparam name="TSource">Type of items in the initial sequence.</typeparam>
+    /// <typeparam name="TResult">Type to which items of the initial sequence map. .</typeparam>
+    /// <param name="source">Sequence which is used to create transformed one.</param>
     /// <param name="func">Function which will be applied to each element of the sequence.</param>
     /// <returns>Transformed sequence.</returns>
-    public static IEnumerable<T> Map<T>(IEnumerable<T> elements, Func<T, T> func)
+    public static IEnumerable<TResult> Map<TSource, TResult>(IEnumerable<TSource> source, Func<TSource, TResult> func)
     {
-        List<T> result = new(elements.Count());
+        List<TResult> result = new(source.Count());
 
-        foreach (var element in elements)
+        foreach (var element in source)
         {
             result.Add(func(element));
         }
 
-        return result.AsEnumerable();
+        return result;
     }
 
     /// <summary>
     /// Creates sequence removing elements from initial sequence for which passed predicate is true.
     /// </summary>
     /// <typeparam name="T">Type of elements in the sequence.</typeparam>
-    /// <param name="elements">Sequence which is used to create transformed one.</param>
+    /// <param name="source">Sequence which is used to create transformed one.</param>
     /// <param name="predicate">Predicate which determines elements to be deleted.</param>
     /// <returns>Transformed sequence.</returns>
-    public static IEnumerable<T> Filter<T>(IEnumerable<T> elements, Predicate<T> predicate)
+    public static IEnumerable<T> Filter<T>(IEnumerable<T> source, Predicate<T> predicate)
     {
-        List<T> result = new(elements.Count());
+        List<T> result = new(source.Count());
 
-        foreach (var element in elements)
+        foreach (var item in source)
         {
-            if (predicate(element))
+            if (predicate(item))
             {
-                result.Add(element);
+                result.Add(item);
             }
         }
 
-        return result.AsEnumerable();
+        return result;
     }
 
     /// <summary>
     /// Calculates result of doing sequence traversal and applying passed function to each element.
     /// </summary>
-    /// <typeparam name="T">Type of elements in the sequence.</typeparam>
-    /// <param name="elements">Sequence which is used to calculate.</param>
+    /// <typeparam name="TSource">Type of items in the sequence.</typeparam>
+    /// <typeparam name="TResult">Type of returned value.</typeparam>
+    /// <param name="source">Sequence which is used to calculate.</param>
     /// <param name="initialValue">Value which is passed to function as left operand at the first call.</param>
     /// <param name="func">Function which is used to calculate.</param>
     /// <returns>Result of doing traversal.</returns>
-    public static T Fold<T>(IEnumerable<T> elements, T initialValue, Func<T, T, T> func)
+    public static TResult Fold<TSource, TResult>(IEnumerable<TSource> source, TResult initialValue, Func<TResult, TSource, TResult> func)
     {
         var result = initialValue;
 
-        foreach (var element in elements)
+        foreach (var element in source)
         {
             result = func(result, element);
         }
